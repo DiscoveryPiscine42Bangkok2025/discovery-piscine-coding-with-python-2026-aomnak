e982226f48de1a280162963c1483a366269ca41a
NongAom <aomnak_aj@hotmail.co.th>
2026-02-11 01:39:47 +0700

    add initial implementations for name and age exercises

---
+++ cell01/ex00/name.py
@@ -0,0 +1,5 @@
+first_name = "Pornteera"
+last_name = "Nakaiy"
+
+print(first_name)
+print(last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,6 @@
+first_name = "Pornteera"
+last_name = "Nakaiy"
+
+whole_name = first_name + " " + last_name
+
+print(whole_name)
