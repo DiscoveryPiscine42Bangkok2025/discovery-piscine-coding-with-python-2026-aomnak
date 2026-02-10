12bb435f967e40837f58e21d1bed25579b2a068a
NongAom <aomnak_aj@hotmail.co.th>
2026-02-11 02:01:48 +0700

    cell02 ex03 multiplication

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,13 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+result = a * b
+
+print(str(a) + " x " + str(b) + " = " + str(result))
+
+if result > 0:
+    print("The result is positive.")
+elif result < 0:
+    print("The result is negative.")
+else:
+    print("The result is positive and negative.")
